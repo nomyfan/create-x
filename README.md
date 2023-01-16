d06bbb63a6e5661a155b8ca68ee47a8b6489c8db
nomyfan <Nomyfan@live.com>
2023-01-16 19:16:45 +0800

    feat: support gitlab

--- README.md
+++ README.md
@@ -8,8 +8,34 @@ cargo install --git https://github.com/nomyfan/create-x
 
 ## Initialize a project from a template
 
+Copy the url in browser address bar targeting the template folder and provide the project name.
+
+### GitHub
+
 ```shell
-# Copy the url in browser address bar targeting the template folder.
-# And provide the project name.
-create-x --url https://github.com/nomyfan/templates/tree/main/vite-react-ts --name vite-react-ts
+create-x \
+--url https://github.com/nomyfan/templates/tree/main/vite-react-ts \
+--name vite-react-ts
+```
+
+```shell
+create-x \
+--url https://your-github-domain.com/nomyfan/templates/tree/main/vite-react-ts \
+--name vite-react-ts \
+--type github
+```
+
+### GitLab
+
+```shell
+create-x \
+--url https://gitlab.com/nomyfan/templates/-/tree/main/vite-react-ts \
+--name vite-react-ts
+```
+
+```shell
+create-x \
+--url https://your-gitlab-domain.com/nomyfan/templates/-/tree/main/vite-react-ts \
+--name vite-react-ts \
+--type gitlab
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,130 @@
-mod github;
-
 use anyhow::Result;
 use clap::Parser;
+use std::{env::temp_dir, path::PathBuf};
 
-lazy_static::lazy_static! {
-    static ref GITHUB_URL_RE: regex::Regex = regex::Regex::new(
-        r"https://github.com/(?P<owner>[^/]+)/(?P<repo>[^/]+)/tree/(?P<refs>[^/]+)/(?P<path>(.+))",
-    ).unwrap();
+#[derive(Clone)]
+enum Type {
+    GitHub,
+    GitLab,
 }
 
-#[derive(Parser, Debug)]
+impl clap::ValueEnum for Type {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::GitHub, Self::GitLab]
+    }
+
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        match self {
+            Self::GitHub => Some(clap::builder::PossibleValue::new("github")),
+            Self::GitLab => Some(clap::builder::PossibleValue::new("gitlab")),
+        }
+    }
+}
+
+#[derive(Parser)]
 #[command()]
 struct Args {
     #[arg(short, long)]
     url: String,
     #[arg(short, long)]
     name: String,
+    #[arg(name = "type", value_enum, short, long)]
+    ty: Option<Type>,
+}
+
+struct Info {
+    owner: String,
+    repo: String,
+    refs: String,
+    path: String,
+    domain: String,
+}
+
+fn parse_url<'a>(url: &'a str, ty: Option<Type>) -> Info {
+    let ty = match ty {
+        Some(ty) => ty,
+        None => {
+            if url.starts_with("https://github.com/") {
+                Type::GitHub
+            } else if url.starts_with("https://gitlab.com/") {
+                Type::GitLab
+            } else {
+                eprintln!(
+                    "Unsupported URL schema(GitHub and GitLab like only), you can tell me the schema type by providing --type argument."
+                );
+                std::process::exit(1);
+            }
+        }
+    };
+
+    match ty {
+        Type::GitHub => {
+            let re = regex::Regex::new(r"https://(?P<domain>[^/]+)/(?P<owner>[^/]+)/(?P<repo>[^/]+)/tree/(?P<refs>[^/]+)/(?P<path>(.+))").unwrap();
+
+            re.captures(url)
+                .map(|caps| {
+                    let owner = caps.name("owner").unwrap().as_str().into();
+                    let repo = caps.name("repo").unwrap().as_str().into();
+                    let refs = caps.name("refs").unwrap().as_str().into();
+                    let path = caps.name("path").unwrap().as_str().into();
+                    let domain = caps.name("domain").unwrap().as_str().into();
+
+                    Info { owner, repo, refs, path, domain }
+                })
+                .expect("The URL doesn't match GitHub's schema")
+        }
+        Type::GitLab => {
+            let re = regex::Regex::new( r"https://(?P<domain>[^/]+)/(?P<owner>[^/]+)/(?P<repo>[^/]+)/-/tree/(?P<refs>[^/]+)/(?P<path>(.+))").unwrap();
+
+            re.captures(url)
+                .map(|caps| {
+                    let owner = caps.name("owner").unwrap().as_str().into();
+                    let repo = caps.name("repo").unwrap().as_str().into();
+                    let refs = caps.name("refs").unwrap().as_str().into();
+                    let path = caps.name("path").unwrap().as_str().into();
+                    let domain = caps.name("domain").unwrap().as_str().into();
+
+                    Info { owner, repo, refs, path, domain }
+                })
+                .expect("The URL doesn't match GitLab's schema")
+        }
+    }
+}
+
+fn fetch_template<'a>(url: &'a str, ty: Option<Type>) -> Result<PathBuf> {
+    let Info { owner, repo, refs, path, domain } = parse_url(url, ty);
+
+    let normalized_domain = domain.replace('.', "_");
+
+    let folder_name = format!("create-x-{normalized_domain}-{owner}-{repo}-{refs}");
+    let clone_dir = temp_dir().join(&folder_name);
+
+    let sh = xshell::Shell::new()?;
+
+    if clone_dir.exists() {
+        sh.change_dir(clone_dir.as_path());
+        xshell::cmd!(sh, "git pull").ignore_stdout().run()?;
+    } else {
+        sh.change_dir(temp_dir());
+        xshell::cmd!(
+            sh,
+            "git clone --depth 1 --branch {refs} git@{domain}:{owner}/{repo}.git {folder_name}"
+        )
+        .ignore_stdout()
+        .run()?;
+    }
+
+    let template_dir = path.split('/').into_iter().fold(clone_dir, |x, y| x.join(y));
+    Ok(template_dir)
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let caps = GITHUB_URL_RE.captures(&args.url).expect("Invalid github url");
-
-    let owner = caps.name("owner").unwrap().as_str();
-    let repo = caps.name("repo").unwrap().as_str();
-    let refs = caps.name("refs").unwrap().as_str();
-    let path = caps.name("path").unwrap().as_str();
-
-    let repo_root_dir = github::fetch(owner, refs, repo)?;
+    let template_dir = fetch_template(&args.url, args.ty)?;
 
     // Copy template into target directory
     {
-        let template_dir = path.split('/').into_iter().fold(repo_root_dir, |x, y| x.join(y));
-
         let mut copy_options = fs_extra::dir::CopyOptions::new();
         let dest_dir = std::path::Path::new(&args.name);
         copy_options.copy_inside = true;
